e8b751157febe47f11e073e4c13019ba42f79bb1
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-03-01 01:26:10 +0300

    fix: add key in list

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -3,13 +3,14 @@ import { Row } from '../rows/rows';
 import { useAppSelector as useSelector } from '../../services/hooks';
 import { getSize } from '../../services/board/board.selectors';
 import styles from './board.module.css';
+import { nanoid } from 'nanoid';
 
 export const Board: FC = () => {
   const size = useSelector(getSize);
   const rows = [];
 
   for (let i = 0; i < size; i++) {
-    rows.push(<Row rowNumber={i} />)
+    rows.push(<Row key={nanoid()} rowNumber={i} />)
   }
 
   return (
--- src/components/rows/rows.tsx
+++ src/components/rows/rows.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { nanoid } from 'nanoid';
 import { useAppSelector as useSelector } from '../../services/hooks';
 import { getSize } from '../../services/board/board.selectors';
 import { Cell } from '../cell/cell';
@@ -9,11 +10,11 @@ export const Row: FC<{ rowNumber: number }> = ({ rowNumber }) => {
   const size = useSelector(getSize);
 
   for (let i = 0 ; i < size; i++) {
-    cells.push(<li><Cell x={i} y={rowNumber} /></li>);
+    cells.push(<li key={nanoid()}><Cell x={i} y={rowNumber} /></li>);
   }
 
   return(
-    <ul className={styles.container}>
+    <ul key={nanoid()} className={styles.container}>
       {
         [ ...cells ]
       }
